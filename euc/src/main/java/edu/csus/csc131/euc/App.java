bdc14fd375621ddb2fe44a40726e560641bc41d0
Pix <pmapanao@csus.edu>
2020-03-29 12:56:43 -0700

    UI Main Window Created

---
+++ euc/src/main/java/edu/csus/csc131/euc/App.java
@@ -0,0 +1,17 @@
+package edu.csus.csc131.euc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        UIwindow app = new UIwindow("Main Window", 600, 600);
+        app.initializeGUI();
+        app.showGUI();
+
+
+    }
+}
