464e3b80b0f17e018c6be727e2a72d0a79bd5711
khoomd <khoomd@ecs.csus.edu>
2020-04-06 14:43:10 -0700

    fix?

--- src/main/java/edu/csus/csc131/euc/App.java
+++ src/main/java/edu/csus/csc131/euc/App.java
@@ -4,70 +4,6 @@ import java.util.Arrays;
 import javax.swing.JFrame;
 import edu.csus.csc131.euc.model.*;
 
-<<<<<<< HEAD
-public class App
-{
-    // constants
-    private static final int RATES_ARRAY_SIZE = 24;
-    private static final float RATES_DEFAULT_OFFPEAK_SUMMER = (float)0.1209;
-    private static final float RATES_DEFAULT_OFFPEAK_NONSUMMER = (float)0.1006;
-    private static final float RATES_DEFAULT_MIDPEAK_SUMMER = (float)0.1671;
-    private static final float RATES_DEFAULT_PEAK_SUMMER = (float)0.2941;
-    private static final float RATES_DEFAULT_PEAK_NONSUMMER = (float)0.1388;
-
-    // Static Main window for application
-    static UIwindow mainwindow = new UIwindow("Main Window", 800, 800);
-
-    // helper method for setting default rates
-    private static float[] setDefaultRates(boolean isSummer){
-        float temp[] = new float[RATES_ARRAY_SIZE];
-        if(isSummer){
-            Arrays.fill(temp, 0, 11, RATES_DEFAULT_OFFPEAK_SUMMER);
-            Arrays.fill(temp, 12, 16, RATES_DEFAULT_MIDPEAK_SUMMER);
-            Arrays.fill(temp, 17, 19, RATES_DEFAULT_PEAK_SUMMER);
-            Arrays.fill(temp, 20, 23, RATES_DEFAULT_MIDPEAK_SUMMER);
-        }else{
-            Arrays.fill(temp, 0, 15, RATES_DEFAULT_OFFPEAK_NONSUMMER);
-            Arrays.fill(temp, 16, 19, RATES_DEFAULT_PEAK_NONSUMMER);
-            Arrays.fill(temp, 20, 23, RATES_DEFAULT_OFFPEAK_NONSUMMER);
-        }
-        return temp;
-    }
-
-    // Initializing the main GUI for application
-    public static void initializeGUI() {
-
-        // Main Window initialization
-        // mainwindow.initializeWindow();
-
-        // Other intializations for UI elements ...
-    }
-
-    public static void main( String[] args )
-    {
-        // input default rates
-        float defaultRates[] = setDefaultRates(true); //just for test. Boolean determines if summer or non summer
-        Rates rates = new Rates(defaultRates);
-        // make instance of the window
-        //UIwindow window1 = new UIwindow("Main Window", 800, 800);
-        // window1.initializeGUI();
-        MainWindow mainWindow = new MainWindow();
-        try{
-            Profile p = new Profile(0);
-            p.setRates(rates);
-            mainWindow.addProfile(p);
-        }
-        catch(Exception e){
-            //do nothing
-        }
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
-        frame.setSize(490, 344);
-        frame.add(mainWindow);
-
-        // make instance of the window
-        //initializeGUI();
-=======
 /* Local Imports */
 import edu.csus.csc131.euc.View.View;
 import edu.csus.csc131.euc.Model.Model; 
@@ -81,7 +17,6 @@ public class App {
         View v = new View();
         Controller c = new Controller(m , v); 
         c.createAndShowGUI();
->>>>>>> origin/ui_MVCPix
 
     }
 
