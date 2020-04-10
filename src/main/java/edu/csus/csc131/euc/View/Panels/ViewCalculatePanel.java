105eb4575e14e7719b2c51cba4d7710d83ece2b3
khoomd <khoomd@ecs.csus.edu>
2020-04-10 16:46:08 -0700

    PB 7, 8, 9, 23. All features not fully implemented, but is working

--- src/main/java/edu/csus/csc131/euc/View/Panels/ViewCalculatePanel.java
+++ src/main/java/edu/csus/csc131/euc/View/Panels/ViewCalculatePanel.java
@@ -18,10 +18,10 @@ public class ViewCalculatePanel{
     private JButton navrightbutton, navleftbutton;
     private JTextField usagetotalday, usagenonpeak, usagemidpeak, usagepeak,
         costtotalday, costnonpeak, costmidpeak, costpeak, totalusage, totalcost,
-        summeroffpeak, summermidpeak, summerpeak, nonsummeroffpeak, nonsummerpeak;
+        summeroffpeak, summermidpeak, summerpeak, nonsummeroffpeak, nonsummerpeak, usagestarttime, usageendtime;
 
     // Labels
-    JLabel summarytitle, usagetitle; 
+    JLabel summarytitle, usagetitle, usagestarttimetitle, usageendtimetitle; 
 
     JLabel [] totaldetails = {
         new JLabel("Total Usage"), 
@@ -62,6 +62,8 @@ public class ViewCalculatePanel{
     public void setUsageNonPeak(JTextField t) { this.usagenonpeak = t; }
     public void setUsageMidPeak(JTextField t) { this.usagemidpeak = t; }
     public void setUsagePeak(JTextField t) { this.usagepeak = t; }
+    public void setUsageStartTime(JTextField t) { this.usagestarttime = t; }
+    public void setUsageEndTime(JTextField t) { this.usageendtime = t; }
 
     // Setters - Usage Details - Cost 
     public void setCostDayTotal(JTextField t) { this.costtotalday = t; }
@@ -91,29 +93,31 @@ public class ViewCalculatePanel{
     public JButton getNavigateLeftButton() { return this.navleftbutton; }
 
     // Getters - Usage Details - Usage 
-    public JTextField setUsageDayTotal() { return this.usagetotalday; }
-    public JTextField setUsageNonPeak() { return this.usagenonpeak; }
-    public JTextField setUsageMidPeak() { return this.usagemidpeak; }
-    public JTextField setUsagePeak() { return this.usagepeak; }
+    public JTextField getUsageDayTotal() { return this.usagetotalday; }
+    public JTextField getUsageNonPeak() { return this.usagenonpeak; }
+    public JTextField getUsageMidPeak() { return this.usagemidpeak; }
+    public JTextField getUsagePeak() { return this.usagepeak; }
+    public JTextField getUsageStartTime() { return this.usagestarttime; }
+    public JTextField getUsageEndTime() { return this.usageendtime; }
     
     // Getters - Usage Details - Cost 
-    public JTextField setCostDayTotal() { return this.costtotalday; }
-    public JTextField setCostNonPeak() { return this.costnonpeak; }
-    public JTextField setCostMidPeak() { return this.costmidpeak; }
-    public JTextField setCostPeak() { return this.costpeak; }
+    public JTextField getCostDayTotal() { return this.costtotalday; }
+    public JTextField getCostNonPeak() { return this.costnonpeak; }
+    public JTextField getCostMidPeak() { return this.costmidpeak; }
+    public JTextField getCostPeak() { return this.costpeak; }
 
     // Getters - Summary Details - Summer 
-    public JTextField setSummerOffPeak() { return this.summeroffpeak; }
-    public JTextField setSummerMidPeak() { return this.summermidpeak; }
-    public JTextField setSummerPeak() { return this.summerpeak; }
+    public JTextField getSummerOffPeak() { return this.summeroffpeak; }
+    public JTextField getSummerMidPeak() { return this.summermidpeak; }
+    public JTextField getSummerPeak() { return this.summerpeak; }
 
     // Getters - Summary Details - Non-Summer
-    public JTextField setNonSummerOffPeak() { return this.nonsummeroffpeak; }
-    public JTextField setNonSummerPeak() { return this.nonsummerpeak; }
+    public JTextField getNonSummerOffPeak() { return this.nonsummeroffpeak; }
+    public JTextField getNonSummerPeak() { return this.nonsummerpeak; }
 
     // Getters - Total Details
-    public JTextField setTotalUsage() { return this.totalusage; }
-    public JTextField setTotalCost() { return this.totalcost; }
+    public JTextField getTotalUsage() { return this.totalusage; }
+    public JTextField getTotalCost() { return this.totalcost; }
 
     /* Functions for ViewCalculate Panel*/ 
 
@@ -130,6 +134,8 @@ public class ViewCalculatePanel{
         /* Instantiate all components of panel here */ 
         // Usage Details 
         this.usagetitle = new JLabel("Usage Details"); 
+        this.usagestarttimetitle = new JLabel("Usage Start Time"); 
+        this.usageendtimetitle = new JLabel("Usage End Time"); 
 
         // Usage Nav Buttons 
         this.navrightbutton = new JButton(">");
@@ -140,6 +146,8 @@ public class ViewCalculatePanel{
         this.usagenonpeak = new JTextField("Usage Non-Peak");
         this.usagemidpeak = new JTextField("Usage Mid-Peak");
         this.usagepeak = new JTextField("Usage Peak");
+        this.usagestarttime = new JTextField("Usage Start Time");
+        this.usageendtime = new JTextField("Usage Start Time");
 
         // Usage Text Fields - Cost
         this.costtotalday = new JTextField("Total Cost (day)");
@@ -170,6 +178,8 @@ public class ViewCalculatePanel{
         this.usagenonpeak.setPreferredSize(DIMENSION_TEXTFIELD); this.usagenonpeak.setEditable(false);
         this.usagemidpeak.setPreferredSize(DIMENSION_TEXTFIELD); this.usagemidpeak.setEditable(false);
         this.usagepeak.setPreferredSize(DIMENSION_TEXTFIELD); this.usagepeak.setEditable(false);
+        this.usagestarttime.setPreferredSize(DIMENSION_TEXTFIELD); this.usagestarttime.setEditable(false);
+        this.usageendtime.setPreferredSize(DIMENSION_TEXTFIELD); this.usageendtime.setEditable(false);
 
         this.costtotalday.setPreferredSize(DIMENSION_TEXTFIELD); this.costtotalday.setEditable(false);
         this.costnonpeak.setPreferredSize(DIMENSION_TEXTFIELD); this.costnonpeak.setEditable(false);
@@ -205,6 +215,10 @@ public class ViewCalculatePanel{
 
         // Usage Details - Title 
         c.gridx = 1; c.gridy = 0; panel.add(this.usagetitle, c); 
+        c.gridx = 2; c.gridy = 0; panel.add(this.usagestarttimetitle, c); 
+        c.gridx = 3; c.gridy = 0; panel.add(this.usagestarttime, c); 
+        c.gridx = 4; c.gridy = 0; panel.add(this.usageendtimetitle, c); 
+        c.gridx = 5; c.gridy = 0; panel.add(this.usageendtime, c); 
 
         // Usage Details - Usage - Labels
         c.gridx = 1; c.gridy = 1; panel.add(this.usagedetails[0], c);
