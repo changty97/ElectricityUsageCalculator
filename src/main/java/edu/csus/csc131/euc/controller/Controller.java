7044465dba18bc469cd80296a2696ecdd3bcbea3
Tyler <changt@ecs.csus.edu>
2020-04-27 23:19:37 -0700

    Update with actionlistener for edit/delete button

--- src/main/java/edu/csus/csc131/euc/controller/Controller.java
+++ src/main/java/edu/csus/csc131/euc/controller/Controller.java
@@ -244,6 +244,82 @@ public class Controller {
             public JButton getButton() { return this.btn; }
           }
     
+          public class ButtonEditor extends DefaultCellEditor {
+            protected JButton button;
+           
+            private String label;
+           
+            private boolean isPushed;
+           
+            public ButtonEditor(JCheckBox checkBox) {
+              super(checkBox);
+              button = new JButton();
+              button.setOpaque(true);
+              button.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println(": Ouch!");
+                    fireEditingStopped();
+                }
+              });
+            }
+           
+            public Component getTableCellEditorComponent(JTable table, Object value,
+                boolean isSelected, int row, int column) {
+              if (isSelected) {
+                button.setForeground(table.getSelectionForeground());
+                button.setBackground(table.getSelectionBackground());
+              } else {
+                button.setForeground(table.getForeground());
+                button.setBackground(table.getBackground());
+              }
+              label = (value == null) ? "" : value.toString();
+              button.setText(label);
+              isPushed = true;
+              return button;
+            }
+           
+            public Object getCellEditorValue() {
+              if (isPushed) {
+                // 
+                // 
+                JOptionPane.showMessageDialog(button, label + ": Ouch!");
+                 System.out.println(label + ": Ouch!");
+              }
+              isPushed = false;
+              return new String(label);
+            }
+           
+            public boolean stopCellEditing() {
+              isPushed = false;
+              return super.stopCellEditing();
+            }
+           
+            protected void fireEditingStopped() {
+              super.fireEditingStopped();
+            }
+          }
+
+          public class ButtonRenderer extends JButton implements TableCellRenderer {
+ 
+            public ButtonRenderer() {
+              setOpaque(true);
+            }
+           
+            public Component getTableCellRendererComponent(JTable table, Object value,
+                boolean isSelected, boolean hasFocus, int row, int column) {
+              if (isSelected) {
+                setForeground(table.getSelectionForeground());
+                setBackground(table.getSelectionBackground());
+              } else {
+                setForeground(table.getForeground());
+                setBackground(UIManager.getColor("Button.background"));
+              }
+              setText((value == null) ? "" : value.toString());
+              return this;
+            }
+          }
+           
+
     class AddEntryListener implements ActionListener {
         @Override
         public void actionPerformed(final ActionEvent e) {
@@ -267,8 +343,10 @@ public class Controller {
                     row[1] = view.getManualInputPanel().getEnterPeriodField().getSelectedItem().toString();
                     row[2] = String.format("%.4f", Float.parseFloat(view.getManualInputPanel().getEnterUsageField().getText()));
                     //Add Edit button
-                    view.getManualInputPanel().getTable().getColumn("Edit").setCellRenderer(Edit);
-                    // Edit.getButton().addActionListener(new EditRowListener());
+                    // view.getManualInputPanel().getTable().getColumn("Edit").setCellRenderer(Edit);
+                    view.getManualInputPanel().getTable().getColumn("Edit").setCellRenderer(new ButtonRenderer());
+                    view.getManualInputPanel().getTable().getColumn("Edit").setCellEditor(new ButtonEditor(new JCheckBox()));
+                    
 
                     //Add Delete Button
                     view.getManualInputPanel().getTable().getColumn("Delete").setCellRenderer(Delete);
